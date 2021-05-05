094d78d3eff305c634990927737df36ef80f4a2d
Danil Gashigullin <dgash2201@yandex.ru>
2021-05-05 23:33:25 +0300

    feat: создать лобби

--- Reports/ClientApp/src/components/App.js
+++ Reports/ClientApp/src/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Lobby from './Lobby';
+import ProtectedRoute from './ProtectedRoute';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function App() {
   return (
     <CurrentUserContext.Provider value={currentUser}>
       <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+
+      <Switch>
+        <Route exact path="/signin">
+          <Lobby>
+          </Lobby>
+        </Route>
+        <Route exact path="/signup">
+          <Lobby>
+          </Lobby>
+        </Route>
+        <ProtectedRoute component={Main} path="/" isLoggedIn={isLoggedIn}>
+        </ProtectedRoute>
+      </Switch>
 
       <Footer />
     </CurrentUserContext.Provider>
