617b56b176f6035ffd2f95c9cabc7daea66eed8b
Danil Gashigullin <dgash2201@yandex.ru>
2021-05-05 23:19:21 +0300

    feat: создать header

--- Reports/Startup.cs
+++ Reports/Startup.cs
@@ -108,8 +108,8 @@ namespace Reports
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Reports v1"));
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Reports v1"));
             }
 
             app.UseHttpsRedirection();
