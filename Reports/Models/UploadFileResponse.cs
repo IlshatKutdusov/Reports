33b6d16e8d721be3c863655927c5b963748d9ec2
SizeR <vip.sanki007@bk.ru>
2021-05-07 18:38:16 +0300

    Small changes in FileService: Now the file upload method returns File Entity

---
+++ Reports/Models/UploadFileResponse.cs
@@ -0,0 +1,15 @@
+﻿using Reports.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reports.Models
+{
+    public class UploadFileResponse : DefaultResponse
+    {
+        public bool Done { get; set; }
+
+        public File? File { get; set; }
+    }
+}
